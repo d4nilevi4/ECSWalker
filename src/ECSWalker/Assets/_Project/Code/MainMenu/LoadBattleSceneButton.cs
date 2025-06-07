84a50f3f9d565bde80c7c51f7ed7fee3e2241fb5
d4nilevi4 <d4nia14@gmail.com>
2025-06-08 00:34:35 +0300

    add load battle scene button in main manu scene

---
+++ src/ECSWalker/Assets/_Project/Code/MainMenu/LoadBattleSceneButton.cs
@@ -0,0 +1,41 @@
+﻿using Cysharp.Threading.Tasks;
+using ECSWalker.Infrastructure;
+using UnityEngine.UI;
+using UnityEngine;
+using Zenject;
+
+namespace ECSWalker.MainMenu
+{
+    [RequireComponent(typeof(Button))]
+    public class LoadBattleSceneButton : MonoBehaviour
+    {
+        private IGameStateMachine _gameStateMachine;
+        private Button _button;
+
+        [Inject]
+        private void Construct(
+            IGameStateMachine gameStateMachine
+        )
+        {
+            _gameStateMachine = gameStateMachine;
+        }
+        
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(OnLoadBattleSceneButtonPressed);
+        }
+
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveListener(OnLoadBattleSceneButtonPressed);
+        }
+
+        private void OnLoadBattleSceneButtonPressed()
+        {
+            _gameStateMachine
+                .Enter<LoadingBattleState, string>(Scenes.BATTLE)
+                .Forget();
+        }
+    }
+}
